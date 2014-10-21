f1b51c11dd33fe3083ef44a2b63f153a2dbe0f3f
Dan Buch <dan@travis-ci.org>
2014-10-21 10:15:29 -0400

    Moving the guts into ./server package before adding realness

--- worker_manager_service.go
+++ worker_manager_service.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"os"
 
-	"github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
-	"github.com/codegangsta/negroni"
-	"github.com/gorilla/mux"
-	"github.com/meatballhat/negroni-logrus"
+	"github.com/travis-pro/worker-manager-service/server"
 )
 
 func main() {
@@ -33,49 +28,5 @@ func main() {
 }
 
 func runServer(c *cli.Context) {
-	srv := newServer(c.String("addr"))
-	srv.Setup()
-	srv.Run()
-}
-
-type server struct {
-	addr string
-	log  *logrus.Logger
-	n    *negroni.Negroni
-	r    *mux.Router
-}
-
-func newServer(addr string) *server {
-	return &server{
-		addr: addr,
-		log:  logrus.New(),
-		n:    negroni.New(),
-		r:    mux.NewRouter(),
-	}
-}
-
-func (srv *server) Setup() {
-	srv.setupRoutes()
-	srv.setupMiddleware()
-}
-
-func (srv *server) Run() {
-	srv.log.WithField("addr", srv.addr).Info("Listening")
-	srv.n.Run(srv.addr)
-}
-
-func (srv *server) setupRoutes() {
-	srv.r.HandleFunc(`/`, srv.handleRoot).Methods("GET").Name("root")
-}
-
-func (srv *server) setupMiddleware() {
-	srv.n.Use(negroni.NewRecovery())
-	srv.n.Use(negronilogrus.NewMiddleware())
-	srv.n.UseHandler(srv.r)
-}
-
-func (srv *server) handleRoot(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text-plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "ohai\n")
+	server.Main(c.String("addr"))
 }
